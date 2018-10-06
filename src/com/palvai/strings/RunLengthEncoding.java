f1c8f1a8b4fe252046d48648b3a8666fbcbe1e66
Sathish Palvai <sathish.palvai@gmail.com>
2018-10-06 07:10:24 -0700

    Update RunLengthEncoding.java

--- src/com/palvai/strings/RunLengthEncoding.java
+++ src/com/palvai/strings/RunLengthEncoding.java
@@ -1,1 +1,24 @@
 
+package com.palvai.strings;
+
+import java.util.regex.Mathcher;
+import java.util.regex.Pattern;
+
+public class RunLengthEncoding {
+ 
+  public static String encode(String inputStr) {
+    StringBuffer enodedString = new StringBuffer();
+    int inputLen  = input.length();
+    for(int i=0;i<inputLen;i++) {
+        int runLen = 1;
+        while(i+1 < inputLen && inputStr.charAt(i) == inputStr.charAt(i+1)) {
+          i++;
+          runLen++;
+        }
+      encodedString.append(runLen);
+      encodedString.append(source.charAt(i));
+      
+    }
+    return encodedString.toString();
+  }
+}
