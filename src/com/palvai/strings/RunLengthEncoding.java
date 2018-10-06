0993d9be97d6a5f764e7d18ab50fdf6f6a8e5d62
Sathish Palvai <sathish.palvai@gmail.com>
2018-10-06 08:14:54 -0700

    Update RunLengthEncoding.java

--- src/com/palvai/strings/RunLengthEncoding.java
+++ src/com/palvai/strings/RunLengthEncoding.java
@@ -15,10 +15,27 @@ public class RunLengthEncoding {
           i++;
           runLen++;
         }
+      encodedString.append(runLen == 1 ? "" : runLen);
+      encodedString.append(source.charAt(i));
+      
+    }
+    return encodedString.toString();
+  }
+ 
+ public static String encodeWithSingleCharacter(String inputStr) {
+    StringBuffer enodedString = new StringBuffer();
+    int inputLen  = input.length();
+    for(int i=0;i<inputLen;i++) {
+        int runLen = 1;
+        while(i+1 < inputLen && inputStr.charAt(i) == inputStr.charAt(i+1)) {
+          i++;
+          runLen++;
+        }
       encodedString.append(runLen);
       encodedString.append(source.charAt(i));
       
     }
     return encodedString.toString();
   }
+ 
 }
